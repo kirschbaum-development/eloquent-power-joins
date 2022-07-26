2dc7d457ff9dfcd908db1712032ba445b643f08e
Dima Kamynin <dima@ociohoteles.com>
2022-07-26 09:56:46 +0200

    run additional tests

---
+++ tests/JoinRelationshipWithJoinTypesTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Kirschbaum\PowerJoins\Tests;
+
+use Kirschbaum\PowerJoins\Tests\Models\Category;
+use Kirschbaum\PowerJoins\Tests\Models\Comment;
+use Kirschbaum\PowerJoins\Tests\Models\Group;
+use Kirschbaum\PowerJoins\Tests\Models\Post;
+use Kirschbaum\PowerJoins\Tests\Models\User;
+use Kirschbaum\PowerJoins\Tests\Models\UserProfile;
+
+class JoinRelationshipWithJoinTypesTest extends TestCase
+{
+
+    /**
+     * $category_1
+     *      |- $post_1_1 (published)
+     *      |- $post_1_2 (published)
+     *
+     * $category_2
+     *      |- $post_2_1 (unpublished)
+     */
+    protected function prepare_test_case_1(){
+        $category_1 = factory(Category::class)->state('with:parent')->create();
+        $category_2 = factory(Category::class)->state('with:parent')->create(); // with no posts assigned
+
+        $post_1_1 = factory(Post::class)->create(['category_id' => $category_1->id, 'user']);
+        $post_1_2 = factory(Post::class)->create(['category_id' => $category_1->id]);
+
+        $post_2_1 = factory(Post::class)->create(['category_id' => $category_2->id, 'published' => false]);
+    }
+
+    /**
+     * @test
+     */
+    public function test_categoreis_inner_join_published_posts() {
+        $this->prepare_test_case_1();
+
+        $categories = Category::joinRelationship('posts', [
+            'posts' => function($join){
+                $join->where('posts.published', true);
+            }
+        ])->get();
+
+        dump($categories->toArray());
+        $this->assertCount(2, $categories);
+    }
+
+    public function test_categoreis_left_join_published_posts() {
+        $this->prepare_test_case_1();
+
+        $categories = Category::joinRelationship('posts', [
+            'posts' => function($join){
+                $join->where('posts.published', true);
+                $join->left();
+            }
+        ])->get();
+
+        dump($categories->toArray());
+        $this->assertCount(3, $categories);
+    }
+
+}

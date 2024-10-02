c3394b6b6607b18a2a111ceb89f002aff738296c
Dan Harrin <git@danharrin.com>
2024-10-02 10:57:30 -0400

    Remove DB setup in test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -21,42 +21,4 @@ class TestCase extends \Orchestra\Testbench\TestCase
     {
         return [PowerJoinsServiceProvider::class];
     }
-
-    /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        // Setup default database to use sqlite :memory:
-        $app['config']->set('database.default', 'testbench');
-        // $app['config']->set('database.default', 'postgres');
-        // $app['config']->set('database.default', 'mysql');
-
-        $app['config']->set('database.connections.testbench', [
-            'driver'   => 'sqlite',
-            'database' => ':memory:',
-            'prefix'   => '',
-        ]);
-
-        $app['config']->set('database.connections.postgres', [
-            'driver'   => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'power-joins'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-        ]);
-
-        $app['config']->set('database.connections.mysql', [
-            'driver'   => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'power-joins'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-        ]);
-    }
 }

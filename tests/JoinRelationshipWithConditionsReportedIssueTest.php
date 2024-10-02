863b2f4483fad6d33cd5f04052e76587b2e0eca4
Luís Dalmolin <luis.nh@gmail.com>
2024-10-02 16:07:43 -0400

    Fixed tests

--- tests/JoinRelationshipWithConditionsReportedIssueTest.php
+++ tests/JoinRelationshipWithConditionsReportedIssueTest.php
@@ -11,39 +11,38 @@ use Kirschbaum\PowerJoins\Tests\Models\UserProfile;
 
 class JoinRelationshipWithConditionsReportedIssueTest extends TestCase {
 
-    public function test_conditions_inside_join_plain_callback() {
+    public function test_conditions_inside_join_plain_callback()
+    {
         $queryBuilder = User::query()->joinRelationship('posts', function ($join) {
             $join->published();
         });
+
         $query = $queryBuilder->toSql();
-        // dump($query);
-        $this->assertStringContainsString('"posts"."published" = ?', $query);
+
+        $this->assertQueryContains('"posts"."published" = ?', $query);
     }
 
-    public function test_conditions_inside_nested_join_callback_array_callback() {
+    public function test_conditions_inside_nested_join_callback_array_callback()
+    {
         $queryBuilder = User::query()->joinRelationship('posts.comments', [
             'posts' => function ($join) {
                 $join->published();
             }
         ]);
-        $query = $queryBuilder->toSql();
-        // dump($query);
-        $this->assertStringContainsString('"posts"."published" = ?', $query);
 
+        $query = $queryBuilder->toSql();
+        $this->assertQueryContains('"posts"."published" = ?', $query);
     }
 
-    public function test_conditions_inside_simple_join_array_callback() {
-        $this->markTestSkipped('[SKIPPED] Reported inconsistent conditioning of joins: https://github.com/kirschbaum-development/eloquent-power-joins/issues/105');
-        return;
-
+    public function test_conditions_inside_simple_join_array_callback()
+    {
         $queryBuilder = User::query()->joinRelationship('posts', [
             'posts' => function ($join) {
                 $join->published();
             }
         ]);
-        $query = $queryBuilder->toSql();
-        // dump($query);
-        $this->assertStringContainsString('"posts"."published" = ?', $query);
-    }
 
+        $query = $queryBuilder->toSql();
+        $this->assertQueryContains('"posts"."published" = ?', $query);
+    }
 }
